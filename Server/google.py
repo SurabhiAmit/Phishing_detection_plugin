305151e4378fae6c6733b270d3040a8b12c1f4f7
Surabhi_Amit_Chembra <surabhivasudev1993@gmail.com>
2020-05-14 16:57:30 -0500

    removed API key for safety

--- Server/google.py
+++ Server/google.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def googlescan(urltoScan):
-	api_key='AIzaSyA13sB-HOUx8ykIGmiRTJ2X-JL6rddY-o0'
+	api_key='<please insert yours>'
 	url = "https://safebrowsing.googleapis.com/v4/threatMatches:find"
 	payload = {'client': {'clientId': "mycompany", 'clientVersion': "0.1"},
 	        'threatInfo': {'threatTypes': ["SOCIAL_ENGINEERING", "MALWARE"],
@@ -15,4 +15,3 @@ def googlescan(urltoScan):
 	#print(r) 
 	return r.json()
 
-#googlescan("https://lubanghitamadadidalamhati.org/myaccount/signin/step1.php?rsjkingdomdisini=session=&c0da7d9c74a503072296f59779a21ebb&dispatch=ab6c0d4b627cbbc78c610d2e83810ec052cd7f4051d17ca84fa25dbbe0f07f752a04d8956c1a9ccea50128534f9bca92797fb5468815b2ca60f71d62")	
